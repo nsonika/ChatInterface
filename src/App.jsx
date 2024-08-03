ef08f6481d1cff4b3ece75fd4b451e0beb959850
Sonika <sonikanagekar47@gmail.com>
2024-08-03 10:33:45 +0530

    remove navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Navbar from './components/chat/Navbar';
 import ChatContainer from './components/chat/ChatContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen bg-gray-100">
-      {/* <Navbar /> */}
       <ChatContainer />
     </div>
   );
